09adc3a15e5a2550cc782641c395f0ee342bcd96
Marta Iachini <iachinimarta@gmail.com>
2022-07-06 16:34:22 +0200

    Inserita if per i numeri multipli sia di 3 che di 5

--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@ for (let i = 1; i <= 100; i++) {
     let boxTesto = i;
     let boxClasse = '';
 
-    if (i % 3 == 0) {
+
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        boxTesto = 'FizzBuzz';
+        boxClasse = 'color-red';
+    } else if (i % 3 == 0) {
         boxTesto = 'Fizz';
         boxClasse = 'color-green';
     } else if (i % 5 == 0) {
@@ -18,4 +22,8 @@ for (let i = 1; i <= 100; i++) {
 }
 
 
+
+// Mi conviene impostare una variabile ad es 'boxtesto' che contiene 'i' piuttosto che lavorare con 'i' direttamente.
+// lasciando la variabile 'boxclasse' vuota posso poi modificarla a seconda della necessità che ho.
+// Meglio definire prima tutte le variabili e le operazioni del caso e poi 'stampare'.
 
