429755c456fcf970793679ebf812bac3ba4bfe10
Marta Iachini <iachinimarta@gmail.com>
2022-07-06 16:25:41 +0200

    Prova con le if - divisibile per 3 - divisibile per 5

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 const boxContainerDom = document.querySelector("div.box-container");
 
+for (let i = 1; i <= 100; i++) {
 
+    let boxTesto = i;
+    let boxClasse = '';
 
-for (let i = 0; i <= 100; i++) {
-    const div = `<div class="box">${i}</div>`;
+    if (i % 3 == 0) {
+        boxTesto = 'Fizz';
+        boxClasse = 'color-green';
+    } else if (i % 5 == 0) {
+        boxTesto = 'Buzz';
+        boxClasse = 'color-yellow';
+    }
+
+    const div = `<div class="box ${boxClasse}">${boxTesto}</div>`;
     boxContainerDom.innerHTML += div;
 }
 
